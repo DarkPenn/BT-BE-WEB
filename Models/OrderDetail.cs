706bdc70e0f8ba9acefa3c258b2c2134d08ad069
DarkPenn <shadowpot915@gmail.com>
2025-11-06 17:58:29 +0700

    Revert "BE8"

--- Models/OrderDetail.cs
+++ Models/OrderDetail.cs
@@ -19,7 +19,7 @@ namespace _24DH110165_MyStore.Models
         public int OrderID { get; set; }
         public int Quantity { get; set; }
         public decimal UnitPrice { get; set; }
-        public Nullable<Decimal> TotalPrice { get; set; }
+    
         public virtual Order Order { get; set; }
         public virtual Product Product { get; set; }
     }
